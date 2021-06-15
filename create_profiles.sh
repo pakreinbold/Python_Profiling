9ad9e2f6ec3faef22c631b2f046fc2c7f5f5a3f6
pakreinbold <patrick.ak.reinbold@gmail.com>
2021-06-15 09:41:07 -0400

    File hierarchy changes.

--- create_profiles.sh
+++ create_profiles.sh
@@ -1,11 +1,12 @@
 # Set up some variables
-mpath="${pythonFile}__memory_profile.txt"
-lpath="${pythonFile}__line_profile.txt"
+mpath="./profiles/${pythonFile}__memory_profile.txt"
+lpath="./profiles/${pythonFile}__line_profile.txt"
 
 # Do the profiling and save to txt files
-python -m memory_profiler "${pythonFile}.py" > $mpath
-kernprof -l "${pythonFile}.py"
-python -m line_profiler "${pythonFile}.py.lprof" > $lpath
+python -m memory_profiler "./scripts_to_profile/${pythonFile}.py" > $mpath
+kernprof -l "./scripts_to_profile/${pythonFile}.py"
+mv "${pythonFile}.py.lprof" "./profiles/${pythonFile}.py.lprof"
+python -m line_profiler "./profiles/${pythonFile}.py.lprof" > $lpath
 
 # Run the plotting script
 python profile_processing.py $mpath $lpath
